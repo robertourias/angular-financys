65a25069083347fd36156c7b62230399ff66a20e
Roberto Nicoletti <roberto.urias@gmail.com>
2020-04-21 13:20:16 -0300

    ajustes de sintaxe

--- src/app/pages/categories/categories-routing.module.ts
+++ src/app/pages/categories/categories-routing.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
-import { CategoryListComponent } from "./category-list/category-list.component";
-import { CategoryFormComponent } from "./category-form/category-form.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CategoryListComponent } from './category-list/category-list.component';
+import { CategoryFormComponent } from './category-form/category-form.component';
 
 const routes: Routes = [
-  { path: "", component: CategoryListComponent },
-  { path: "new", component: CategoryFormComponent },
-  { path: ":id/edit", component: CategoryFormComponent }
+  { path: '', component: CategoryListComponent },
+  { path: 'new', component: CategoryFormComponent },
+  { path: ':id/edit', component: CategoryFormComponent }
 ];
 
 @NgModule({
