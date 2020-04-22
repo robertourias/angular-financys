bdff2739f9f21af2e0bef92d59084e284aea5405
Roberto Nicoletti <roberto.urias@gmail.com>
2020-04-22 20:26:34 -0300

    atualizando html da lstagem de categorias e implementando o excluir

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { CategoryService } from "../shared/category.service";
+import { Category } from "../shared/category.model";
 
 @Component({
-  selector: 'app-category-list',
-  templateUrl: './category-list.component.html',
-  styleUrls: ['./category-list.component.css']
+  selector: "app-category-list",
+  templateUrl: "./category-list.component.html",
+  styleUrls: ["./category-list.component.css"],
 })
 export class CategoryListComponent implements OnInit {
+  categories: Category[];
 
-  constructor() { }
+  constructor(private categoryService: CategoryService) {}
 
   ngOnInit() {
+    this.categoryService.getAll().subscribe(
+      (categories) => {
+        this.categories = categories;
+      },
+      (error: any) => {
+        alert("Erro ao carregar a lista: " + error);
+      }
+    );
   }
 
+  delete(category: Category): void {
+    const mustDelete = confirm("Deseja realmente excluir este item?");
+
+    if (mustDelete) {
+      this.categoryService.delete(category.id).subscribe(
+        () => {
+          this.categories = this.categories.filter((elem) => elem !== category);
+        },
+        () => {
+          alert("Erro ao tentar excluir.");
+        }
+      );
+    }
+  }
 }
