6f45503f98c7fd5862cab68956b68490aaf95759
Roberto Nicoletti <roberto.urias@gmail.com>
2020-05-04 20:51:18 -0300

    Organizando core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,15 @@
-import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { HttpClientModule } from "@angular/common/http";
-
-import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
-import { InMemoryDatabase } from "./in-memory-database";
 import { TesteComponentComponent } from "./teste-component/teste-component.component";
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [AppComponent, TesteComponentComponent],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
+    CoreModule,
     AppRoutingModule,
-    HttpClientModule,
-    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase),
   ],
   providers: [],
   bootstrap: [AppComponent],
