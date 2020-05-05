95ad8da5433da82dd8370ab571c7238acd1a7860
Roberto Nicoletti <roberto.urias@gmail.com>
2020-05-04 21:02:51 -0300

    criando a base model

--- src/app/pages/entries/shared/entry.model.ts
+++ src/app/pages/entries/shared/entry.model.ts
@@ -1,6 +1,7 @@
 import { Category } from '../../categories/shared/category.model';
+import { BaseResourceModel } from 'src/app/shared/models/base-resource.model';
 
-export class EntryModel {
+export class EntryModel extends BaseResourceModel {
   constructor(
     public id?: number,
     public name?: string,
@@ -11,7 +12,9 @@ export class EntryModel {
     public paid?: boolean,
     public categoryId?: number,
     public category?: Category
-  ) {}
+  ) {
+    super();
+  }
 
   static types = {
     expense: 'Despesa',
