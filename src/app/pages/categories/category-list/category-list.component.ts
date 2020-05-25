1c1f78f02dc0e4958845cc4368a7d764fe3592b6
Roberto Nicoletti <roberto.nicoletti@picpay.com>
2020-05-24 21:00:47 -0300

    categoryList refactor

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,40 +1,16 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 import { CategoryService } from "../shared/category.service";
 import { Category } from "../shared/category.model";
+import { BaseResourceListComponent } from 'src/app/shared/components/base-resource-list/base-resource-list.component';
 
 @Component({
   selector: "app-category-list",
   templateUrl: "./category-list.component.html",
   styleUrls: ["./category-list.component.css"],
 })
-export class CategoryListComponent implements OnInit {
-  categories: Category[];
+export class CategoryListComponent extends BaseResourceListComponent<Category> {
 
-  constructor(private categoryService: CategoryService) {}
-
-  ngOnInit() {
-    this.categoryService.getAll().subscribe(
-      (categories) => {
-        this.categories = categories;
-      },
-      (error: any) => {
-        alert("Erro ao carregar a lista: " + error);
-      }
-    );
-  }
-
-  delete(category: Category): void {
-    const mustDelete = confirm("Deseja realmente excluir este item?");
-
-    if (mustDelete) {
-      this.categoryService.delete(category.id).subscribe(
-        () => {
-          this.categories = this.categories.filter((elem) => elem !== category);
-        },
-        () => {
-          alert("Erro ao tentar excluir.");
-        }
-      );
-    }
+  constructor(private categoryService: CategoryService) {
+    super(categoryService);
   }
 }
