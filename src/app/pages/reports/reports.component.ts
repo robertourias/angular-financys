adf6f295cd28622d465bee3d02460acbb2469ead
Roberto Nicoletti <roberto.nicoletti@picpay.com>
2020-05-27 20:07:53 -0300

    refactor setChart

--- src/app/pages/reports/reports.component.ts
+++ src/app/pages/reports/reports.component.ts
@@ -42,6 +42,7 @@ export class ReportsComponent implements OnInit {
       .subscribe(categories => this.categories = categories);
   }
 
+
   generateReports(){
     const month = this.mouth.nativeElement.value;
     const year = this.year.nativeElement.value;
@@ -50,7 +51,68 @@ export class ReportsComponent implements OnInit {
       alert('Você precisa selecionar o Mês e o Ano para gerar os relatórios');
     }
     else {
-      this.entryService.getByMonthAndYear(month, year);
+      this.entryService.getByMonthAndYear(month, year).subscribe(this.setValues.bind(this));
+    }
+  }
+
+
+  private setValues(entries: EntryModel[]) {
+    this.entries= entries;
+    this.calculateBalance();
+    this.setChartData();
+  }
+
+
+  private calculateBalance() {
+    let expenseTotal = 0;
+    let revenueTotal = 0;
+
+    this.entries.forEach(entry => {
+      if(entry.type === 'revenue')
+        revenueTotal += currecyFormatter.unformat(entry.amount, { code: 'BRL' });
+
+      else
+        expenseTotal += currecyFormatter.unformat(entry.amount, { code: 'BRL' });
+    });
+
+    this.expenseTotal = currecyFormatter.format(expenseTotal, { code: 'BRL' });
+    this.revenueTotal = currecyFormatter.format(revenueTotal, { code: 'BRL' });
+    this.balance = currecyFormatter.format(revenueTotal - expenseTotal, { code: 'BRL'});
+  }
+
+
+  private setChartData() {
+    this.revenueChartData = this.getChartData('revenue', 'Gráfico de Receitas', '#9CCC65');
+    this.expenseChartData = this.getChartData('expense', 'Gráfico de Despesas', '#e03131');
+  }
+
+
+  private getChartData(entryType: string, title: string, color: string) {
+    const chartData = [];
+    this.categories.forEach(category => {
+      // Filtrando lançamentos pela categoria e tipo
+      const filteredEntries = this.entries.filter(entry => entry.category == category.id && entry.type == entryType);
+
+      // Só mostrar categorias no grafico que tenham lançamentos
+      if(filteredEntries.length> 0) {
+        const totalAmount = filteredEntries.reduce(
+          (total, entry) => total + currecyFormatter.format(entry.amount, { code: 'BRL' }), 0
+        );
+
+        chartData.push({
+          categoryName: category.name,
+          totalAmount: totalAmount
+        });
+      }
+    });
+
+    return {
+      labels: chartData.map(item => item.categoryName),
+      datasets: [{
+        label: title,
+        backgroundColor: color,
+        data: chartData.map(item => item.totalAmount)
+      }]
     }
   }
 
